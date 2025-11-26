67c4a01d51e7eb34af7df711d98312051c66a780
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2025-11-26 23:17:24 +0100

    Add German translation

---
+++ translations/harbour-pipes-de.ts
@@ -0,0 +1,141 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>PipesPage</name>
+    <message>
+        <location filename="../qml/pages/PipesPage.qml" line="69"/>
+        <source>Zoom</source>
+        <translation>Zoom</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PipesPage.qml" line="127"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PipesPage.qml" line="135"/>
+        <source>Scramble pipes</source>
+        <translation>Rohre mischen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PipesPage.qml" line="136"/>
+        <source>Scrambling the pipes</source>
+        <translation>Rohre werden gemischt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PipesPage.qml" line="140"/>
+        <source>New game</source>
+        <translation>Neues Spiel</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PipesPage.qml" line="141"/>
+        <source>Initializing new game</source>
+        <translation>Neues Spiel wird gestartet</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="19"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="22"/>
+        <source>Increasing difficulty</source>
+        <translation>Steigende Schwierigkeit</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="24"/>
+        <source>Difficulty will increase whenever you complete a level</source>
+        <translation>Die Schwierigkeit wird gesteigert, sobald Sie ein Level gelöst haben.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="25"/>
+        <source>You must increase difficulty yourself in this settings page</source>
+        <translation>Sie müssen die Schwierigkeit hier auf der Einstellungsseite manuell einstellen.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="31"/>
+        <source>Horizontal dimension</source>
+        <translation>Horizontale Grösse</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="41"/>
+        <source>Vertical dimension</source>
+        <translation>Vertikale Grösse</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/pages/Settings.qml" line="54"/>
+        <source>Warning: the selected size exceeds the recommended maximum of %n tiles. The game may become really slow.</source>
+        <translation>
+            <numerusform>Achtung: Das Spielfeld ist grösser als %n Feld. Das Spiel könnte sehr langsam werden.</numerusform>
+            <numerusform>Achtung: Das Spielfeld ist grösser als %n Felder. Das Spiel könnte sehr langsam werden.</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="75"/>
+        <source>Clear saved progress and settings</source>
+        <translation>Fortschritt und Einstellungen zurücksetzen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="77"/>
+        <source>Clearing all databases</source>
+        <translation>Alle Daten werden zurückgesetzt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="85"/>
+        <source>Clear saved progress</source>
+        <translation>Fortschritt zurücksetzen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="87"/>
+        <source>Clearing saved progress database</source>
+        <translation>Fortschritt wird zurückgesetzt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="95"/>
+        <source>Reset settings</source>
+        <translation>Einstellungen zurücksetzen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="97"/>
+        <source>Resetting settings</source>
+        <translation>Einstellungen werden zurückgesetzt</translation>
+    </message>
+</context>
+<context>
+    <name>WinPage</name>
+    <message>
+        <location filename="../qml/pages/WinPage.qml" line="44"/>
+        <source>Puzzle completed!</source>
+        <translation>Level geschafft!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WinPage.qml" line="45"/>
+        <source>Next puzzle</source>
+        <translation>Neues Spiel</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WinPage.qml" line="46"/>
+        <source>Back</source>
+        <translation>Zurück</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WinPage.qml" line="49"/>
+        <source>Solution</source>
+        <translation>Lösung</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WinPage.qml" line="84"/>
+        <source>Your time:</source>
+        <translation>Ihre Zeit:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WinPage.qml" line="96"/>
+        <source>Best time:</source>
+        <translation>Beste Zeit:</translation>
+    </message>
+</context>
+</TS>
